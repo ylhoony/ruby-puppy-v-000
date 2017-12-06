4772f6f775185a36682bfd1c8b5dda9fef121d91
Hoon Lee <yl.hoony@gmail.com>
2017-12-06 19:31:22 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|e| puts "#{e.name}" }
+  end
+
+end
